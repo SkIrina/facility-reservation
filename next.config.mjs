e00a8fffb0d9d8030f32d5655f3a53a88fa3b50b
SkIrina <skudnovaira@gmail.com>
2024-12-10 20:31:32 +0900

    fix: set redirect from root page to ENG version

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,15 @@
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/en",
+        permanent: true,
+      },
+    ];
+  },
   images: {
     domains: ["localhost"],
   },
